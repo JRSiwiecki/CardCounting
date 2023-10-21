fea9918e2a802b3cba2ce51b0ef5c15a30ce2b7d
JRSiwiecki <JRSiwiecki@gmail.com>
2023-10-21 14:45:08 -0700

    Rename buttons again, add view binding

--- app/src/main/java/com/bignerdranch/android/cardcounting/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/cardcounting/MainActivity.kt
@@ -2,11 +2,25 @@ package com.bignerdranch.android.cardcounting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.bignerdranch.android.cardcounting.databinding.ActivityMainBinding
+
+private const val TAG = "Main"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.practiceModeButton.setOnClickListener {
+            Log.d(TAG, "practice mode")
+        }
+
+        binding.hardModeButton.setOnClickListener {
+            Log.d(TAG, "hard mode")
+        }
     }
 }
