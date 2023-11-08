823e67abac5a2efdfd58a6da5aa82c4f094fc55d
RAKKII-san <schuang2001@gmail.com>
2023-11-07 16:11:48 -0800

    (Laptop) Add Card and GameViewModel classes to work on later

---
+++ app/src/main/java/com/bignerdranch/android/cardcounting/Card.kt
@@ -0,0 +1,5 @@
+package com.bignerdranch.android.cardcounting
+
+data class Card(
+    val cardValue: Int
+) {}
---
+++ app/src/main/java/com/bignerdranch/android/cardcounting/GameViewModel.kt
@@ -0,0 +1,8 @@
+package com.bignerdranch.android.cardcounting
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+class GameViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+
+}
