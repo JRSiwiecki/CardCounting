28407318a5d61394d70fb4747649586292be625b
JRSiwiecki <JRSiwiecki@gmail.com>
2023-12-03 17:41:50 -0800

    Add ColorStateLists using colors from colors.xml, add functionality to nextCard button, track if user answers correctly.

--- app/src/main/java/com/bignerdranch/android/cardcounting/PracticeActivity.kt
+++ app/src/main/java/com/bignerdranch/android/cardcounting/PracticeActivity.kt
@@ -1,37 +1,60 @@
 package com.bignerdranch.android.cardcounting
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.bignerdranch.android.cardcounting.databinding.ActivityPracticeBinding
 
 class PracticeActivity: AppCompatActivity() {
     private lateinit var binding: ActivityPracticeBinding
     private lateinit var deck: Deck
-    private var count: Int = 0
     private lateinit var countdownTimer: CountDownTimerViewModel
-    private lateinit var activeCard: CardView
+    private lateinit var activeCard: Card
+    private lateinit var cardView: CardView
+    private lateinit var defaultColor: ColorStateList
+    private lateinit var correctColor: ColorStateList
+    private lateinit var disabledColor: ColorStateList
+
+    private var count: Int = 0
+    private var cardsShown: Int = 0
+    private var cardsCorrect: Int = 0
+
     private val cardTimerDuration: Long = 5000
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPracticeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        defaultColor = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.purple_500))
+        correctColor = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.green))
+        disabledColor = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.gray))
+
         deck = Deck.Builder().build()
 
-        activeCard = findViewById(R.id.activecard)
+        cardView = findViewById(R.id.activecard)
 
         updateActiveCard()
 
-        startCountdown(5000)
+        startCountdown(cardTimerDuration)
 
         updateCountText()
 
         binding.pluscount.setOnClickListener { next(1) }
         binding.minuscount.setOnClickListener { next(-1) }
         binding.nocount.setOnClickListener { next(0) }
+
+        binding.nextCard.setOnClickListener {
+            disableNextCardButton()
+            updateActiveCard()
+            resetCountButtons()
+            startCountdown(cardTimerDuration)
+        }
     }
 
     private fun startCountdown(duration: Long) {
@@ -72,7 +95,7 @@ class PracticeActivity: AppCompatActivity() {
 
         startCountdown(5000)
 
-        updateActiveCard()
+        evaluatePlayerAnswer(false, delta)
 
         //Can add card logic/switches here
     }
@@ -81,21 +104,109 @@ class PracticeActivity: AppCompatActivity() {
         binding.count.text = "Count: $count"
     }
 
+    /**
+     * Updates activeCard using getCardFromDeck,
+     * updates cardView accordingly,
+     * increments cardsShown.
+     */
     private fun updateActiveCard() {
-
         // Get next card from deck
-        val nextCard = getCardFromDeck()
+        activeCard = getCardFromDeck()
 
         // Update custom view
-        activeCard.setSymbols(
-            nextCard.rank.symbol,
-            nextCard.suit.symbol.toString(),
-            nextCard.rank.symbol,
-            nextCard.suit.symbol.toString()
+        cardView.setSymbols(
+            activeCard.rank.symbol,
+            activeCard.suit.symbol.toString(),
+            activeCard.rank.symbol,
+            activeCard.suit.symbol.toString()
         )
+
+        cardsShown += 1
     }
+
+    /**
+     * Returns a new card from the deck, or the placeholder 2 of Spades.
+     *
+     * @return A variable of type Card.
+     */
     private fun getCardFromDeck(): Card {
-        // Get card and return or return placeholder card
         return deck.dealCard() ?: Card(Suit.SPADES, Rank.TWO)
+    }
+
+    /**
+     * Evaluates player answer and updates UI accordingly.
+     * Tracks if userIsCorrect to increment cardsCorrect, updates timer to display either:
+     * "Time Over" if user ran out of time, or "Correct!" or "Incorrect!" depending on if user
+     * got the count right. Disables count buttons, and changes their background tints depending
+     * on if user was correct or not. Also enables and makes nextCard button visible.
+     */
+    private fun evaluatePlayerAnswer(ranOutOfTime: Boolean, answer: Int) {
+        var correctAnswer = 0
+        var userIsCorrect = false
+
+        // Evaluate if user answer was correct or not
+        when (activeCard.rank.value) {
+            in 2..6 -> {
+                userIsCorrect = (answer == 1)
+                correctAnswer = 1
+            }
+            in 7..9 -> {
+                userIsCorrect = (answer == 0)
+                correctAnswer = 0
+            }
+            else -> {
+                userIsCorrect = (answer == -1)
+                correctAnswer = -1
+            }
+        }
+
+        binding.timer.text = if (ranOutOfTime) "Timer Over!" else if (userIsCorrect)  "Correct!" else "Incorrect!"
+
+        // Keep track of number of correct answers
+        if (userIsCorrect) {
+            cardsCorrect += 1
+        }
+
+        // Disable count buttons
+        binding.pluscount.isEnabled = false
+        binding.nocount.isEnabled = false
+        binding.minuscount.isEnabled = false
+
+        // Set disabled color for all buttons
+        binding.minuscount.backgroundTintList = disabledColor
+        binding.nocount.backgroundTintList = disabledColor
+        binding.pluscount.backgroundTintList = disabledColor
+
+        // Highlight correct button green
+        when (correctAnswer) {
+            -1 -> binding.minuscount.backgroundTintList = correctColor
+            0 -> binding.nocount.backgroundTintList = correctColor
+            1 -> binding.pluscount.backgroundTintList = correctColor
+        }
+
+        // Display and enable nextCard button
+        binding.nextCard.isVisible = true
+        binding.nextCard.isEnabled = true
+    }
+
+    /**
+     * Disables the button for pulling the next card.
+     */
+    private fun disableNextCardButton() {
+        binding.nextCard.isVisible = false
+        binding.nextCard.isEnabled = false
+    }
+
+    /**
+     * Re-enable buttons, changes their background colors back to default (purple).
+     */
+    private fun resetCountButtons() {
+        binding.pluscount.isEnabled = true
+        binding.nocount.isEnabled = true
+        binding.minuscount.isEnabled = true
+
+        binding.pluscount.backgroundTintList = defaultColor
+        binding.nocount.backgroundTintList = defaultColor
+        binding.minuscount.backgroundTintList = defaultColor
     }
 }
