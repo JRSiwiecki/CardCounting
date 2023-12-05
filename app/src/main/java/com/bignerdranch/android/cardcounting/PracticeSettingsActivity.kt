442d29d84ab1d6aa241309363fa95e31a2de74c5
JRSiwiecki <JRSiwiecki@gmail.com>
2023-12-05 15:22:55 -0800

    Add doc comments, fix bug if timePerCard was not entered.

--- app/src/main/java/com/bignerdranch/android/cardcounting/PracticeSettingsActivity.kt
+++ app/src/main/java/com/bignerdranch/android/cardcounting/PracticeSettingsActivity.kt
@@ -14,8 +14,12 @@ class PracticeSettingsActivity: AppCompatActivity() {
 
         binding.startPracticeButton.setOnClickListener {
             val intent = Intent(this, PracticeActivity::class.java)
+
+            // Small check in case timePerCard was not entered by default
+            val timePerCard = binding.timePerCard.text.toString().toIntOrNull() ?: 5
+
             intent.putExtra("NUMBER_OF_DECKS", binding.numberOfDecks.value)
-            intent.putExtra("TIME_PER_CARD", binding.timePerCard.text.toString().toInt())
+            intent.putExtra("TIME_PER_CARD", timePerCard)
             intent.putExtra("CHALLENGE_TYPE", mapChallengeType(binding.challengeType.checkedRadioButtonId))
             startActivity(intent)
         }
@@ -24,6 +28,12 @@ class PracticeSettingsActivity: AppCompatActivity() {
         binding.numberOfDecks.minValue = 1
     }
 
+    /**
+     * Maps radio buttons to their corresponding ChallengeType.
+     *
+     * @param radioButtonId Int. Holds the ID for a corresponding Radio Button.
+     * @return ChallengeType. Returns the corresponding ChallengeType of the radio button selected.
+     */
     private fun mapChallengeType(radioButtonId: Int): ChallengeType {
         return when (radioButtonId) {
             R.id.easy_button -> ChallengeType.EASY
