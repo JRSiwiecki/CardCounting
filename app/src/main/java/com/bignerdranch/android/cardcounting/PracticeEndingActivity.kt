af9ec0961ea31da7f8ba50f7efbe2f86fd27a855
austinP2 <119561083+austinP2@users.noreply.github.com>
2023-12-06 17:03:20 -0800

    Update PracticeEndingActivity.kt

--- app/src/main/java/com/bignerdranch/android/cardcounting/PracticeEndingActivity.kt
+++ app/src/main/java/com/bignerdranch/android/cardcounting/PracticeEndingActivity.kt
@@ -3,10 +3,12 @@ package com.bignerdranch.android.cardcounting
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.bignerdranch.android.cardcounting.databinding.ActivityPracticeEndingBinding
 
 class PracticeEndingActivity: AppCompatActivity() {
     private lateinit var binding: ActivityPracticeEndingBinding
+    private var challengeType: ChallengeType = ChallengeType.EASY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +19,14 @@ class PracticeEndingActivity: AppCompatActivity() {
         val finalScore = intent.getIntExtra("FINAL_SCORE", 0)
         val correctAnswers = intent.getIntExtra("CORRECT_ANSWERS", 0)
         val correctCount = intent.getIntExtra("CORRECT_COUNT", 0)
+        challengeType = intent.getSerializableExtra("CHALLENGE_TYPE") as ChallengeType
+
+
+        if(challengeType == ChallengeType.HARD){
+            binding.answerCount.isVisible = false
+            binding.textView7.isVisible = false
+        }
+
 
         // Display the results in TextViews
         binding.answerCount.text = correctAnswers.toString()
